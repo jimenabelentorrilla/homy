b8e3095830ad317adcc5c83de73e0951b7e2903a
Jimena Belen <jiment4@hotmail.com>
2024-04-12 21:54:48 -0300

    [add] Nav

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Nav } from "./componentes/Nav";
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,24 @@
+import Logo from "..//assets/homy-06.png";
+
+export const Nav = () => {
+  return (
+    <>
+        <div className="cnt-nav">
+            <div>
+                <img src={Logo}/>
+            </div>
+            <ul className="ul-flex">
+                <li>Home</li>
+                <li>Productos</li>
+                <li>¿Como funciona?</li>
+                <li>Faqs</li>
+                <li>Contact</li>
+            </ul>
+            <div className="cnt-flex">
+                <p>Ingresar</p>
+                <button>Comenzar</button>
+            </div>
+        </div>
+    </>
+  )
+}
