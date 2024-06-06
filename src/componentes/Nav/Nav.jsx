257b1ce0794d95c09212747c5143e09d401b6db0
Jimena Belen <jiment4@hotmail.com>
2024-06-06 19:32:19 -0300

    wd

--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -12,45 +12,40 @@ import { ModalSignUp } from "./ModalSignUp";
 
 export const Nav = () => {
 
-    const { AosInit, openSignIn, handleOpenSignIn,
+    const { openSignIn, handleOpenSignIn,
             handleCloseSignIn, onSignUp, openSignUp,
             handleOpenSignUp, handleCloseSignUp, onSignIn } = useContext(AppContext);
    
-    AosInit()
-
   return (
     <>
-        <nav>
-            <div className="cnt-nav" data-aos="fade-down" data-aos-duration="2000" id="nav">
-                <div>
-                    <NavLink to="/"><img className="logo" src={Logo}/></NavLink>
-                </div>
-                <ul className="ul-flex">
-                    <a href="#"><li>Home</li></a>
-                    <a href="#productos"><li>Productos</li></a>
-                    <a href="#como"><li>¿Cómo funciona?</li></a>
-                    <a href="#faqs"><li>Faqs</li></a>
-                    <a href="#contacto"><li>Contacto</li></a>
-                </ul>
-                <div className="cnt-flex">
-                    <NavLink to="/signIn"><p onClick={handleOpenSignIn}>Ingresar</p></NavLink>
-                    <NavLink to="/signUp"><button onClick={handleOpenSignUp}>Comenzar</button></NavLink> 
-                </div>   
+        <nav className="cnt-nav" id="nav">
+            <div>
+                <NavLink to="/"><img className="logo" src={Logo}/></NavLink>
             </div>
+            <ul className="ul-flex">
+                <a href="#"><li>Home</li></a>
+                <a href="#productos"><li>Productos</li></a>
+                <a href="#como"><li>¿Cómo funciona?</li></a>
+                <a href="#faqs"><li>Faqs</li></a>
+                <a href="#contacto"><li>Contacto</li></a>
+            </ul>
+            <div className="cnt-flex">
+                <NavLink to="/signIn"><p onClick={handleOpenSignIn}>Ingresar</p></NavLink>
+                <NavLink to="/signUp"><button onClick={handleOpenSignUp}>Comenzar</button></NavLink> 
+            </div>   
         </nav>
-        
         <Modal
             open={openSignIn}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             onClose={handleCloseSignIn}>
                 <Box>
-                        <ModalSignIn 
-                            onClose={handleCloseSignIn}
-                            handleOpenSignUp={handleOpenSignUp}
-                            handleCloseSignIn={handleCloseSignIn}
-                            onSignUp={onSignUp} 
-                        />
+                    <ModalSignIn 
+                        onClose={handleCloseSignIn}
+                        handleOpenSignUp={handleOpenSignUp}
+                        handleCloseSignIn={handleCloseSignIn}
+                        onSignUp={onSignUp} 
+                    />
                 </Box>
         </Modal>  
         <Modal
